7bea05aff25ea2cfb6f863d610b361c3c8fd8f8e
manavinsane <107745711+manavinsane@users.noreply.github.com>
2023-08-08 11:59:32 +0530

    Add files via upload

---
+++ Predicate/mypredicate.java
@@ -0,0 +1,12 @@
+import java.util.function.Predicate;
+
+public class mypredicate {
+    public static void main(String[] args) {
+        Predicate<String> p = x -> x.toLowerCase().charAt(0) == 's';
+        Predicate<String> s = x -> x.toLowerCase().charAt(x.length()-1) == 'i';
+        Predicate m = p.or(s);
+        Predicate pr = Predicate.isEqual("Sanskruti");
+        System.out.println(pr.test("Sanskruti"));
+        }
+
+    }
